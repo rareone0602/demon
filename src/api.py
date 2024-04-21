6c7bb1a103b5c59fd38450020611a4bfc14f6a86
rareone0602 <rareone0602@gmail.com>
2024-04-21 14:48:31 +0000

    more align on sdxl

--- src/api.py
+++ src/api.py
@@ -5,16 +5,7 @@ from karras import LatentSDEModel
 from utils import get_condition
 from config import DEVICE, DTYPE
 
-latent_sde = LatentSDEModel(beta=0).to(device=DEVICE, dtype=DTYPE)
-
-@torch.inference_mode()
-def get_f_g(t, x, prompts):
-    f_emp, g_emp = latent_sde(t, x, empty_text_embedding)
-    f1 = 0
-    for text_embedding, cfg in prompts:
-        f, _ = latent_sde(t, x, text_embedding)
-        f1 += (f - f_emp) * cfg
-    return f_emp + f1, g_emp
+latent_sde = LatentSDEModel(beta=0).to(DEVICE).to(DTYPE)
 
 class OdeModeContextManager:
     def __enter__(self):
@@ -28,124 +19,157 @@ def duplicate_condition(conds, n):
         "encoder_hidden_states": conds["encoder_hidden_states"].repeat(n, 1, 1), 
     }
 
+
+def _get_f_g(t, x, prompts):
+    conds = prompts['conditions']
+    cfgs = prompts['cfgs']
+    N, C = x.shape[0], len(cfgs) + 1
+    if N > 1:
+        conds = duplicate_condition(conds, x.shape[0])
+    fs, g = latent_sde(
+        t, 
+        x.repeat_interleave(C, dim=0), 
+        conds
+    )
+
+    all_f = []
+    for j in range(N):
+        f = fs[(j + 1) * C - 1] + \
+            sum((fs[j * C + i] - fs[(j + 1) * C - 1]) * cfg for i, cfg in enumerate(cfgs))
+        all_f.append(f)
+    return torch.stack(all_f), g
+
 @torch.inference_mode()
-def sde_step(x, next_t, t, prompts, z):
+def get_f_g(t, x, prompts):
+    MAX_CHUNK_SIZE = 16
+    N = x.shape[0]
+    all_fs = []
+    for i in range(0, N, MAX_CHUNK_SIZE):
+        chunk = x[i:min(i+MAX_CHUNK_SIZE, N)]
+        fs, g = _get_f_g(t, chunk, prompts) # g is assumed to be the same for all elements in the chunk
+        all_fs.append(fs)
+    return torch.cat(all_fs), g
+
+@torch.inference_mode()
+def sde_step(x, t, prev_t, prompts, z):
     assert x.shape[0] == 1
     # Note: z may have more batch dimensions than x
-    dt = next_t - t
+    dt = t - prev_t
     rand_term = z * torch.sqrt(torch.abs(dt))
-
-    f1, g1 = get_f_g(t, x, prompts)
+    f1, g1 = get_f_g(prev_t, x, prompts)
     x_pred = x + f1 * dt + g1 * rand_term # Expand x to match z's shape
-    f2, g2 = get_f_g(next_t, x_pred, prompts)
+    f2, g2 = get_f_g(t, x_pred, prompts)
     x = x + 0.5 * (f1 + f2) * dt + 0.5 * (g1 + g2) * rand_term
     return x
 
 @torch.inference_mode()
-def odeint_rest(x, start_t, ts, prompts):
-    if len(ts) > 25:
-        ts = latent_sde.get_timesteps(25, start_time=ts[0])
+def ode_step(x, t, prev_t, prompts):
+    dt = t - prev_t
+    f1, _ = get_f_g(prev_t, x, prompts)
+    x_pred = x + f1 * dt
+    f2, _ = get_f_g(t, x_pred, prompts)
+    x = x + 0.5 * (f1 + f2) * dt
+    return x
+
+@torch.inference_mode()
+def odeint_rest(x, start_t, ts, prompts, max_ode_steps=15):
+    if len(ts) > max_ode_steps:
+        ts = latent_sde.get_timesteps(max_ode_steps, sigma_max=ts[0], sigma_min=ts[-1])
     prev_t = start_t
     for t in ts:
-        dt = t - prev_t
-        f1, _ = get_f_g(prev_t, x, prompts)
-        x_pred = x + f1 * dt
-        f2, _ = get_f_g(t, x_pred, prompts)
-        x = x + 0.5 * (f1 + f2) * dt
+        x = ode_step(x, t, prev_t, prompts)
         prev_t = t
     return x
 
 @torch.inference_mode()
-def odeint(x, text_weight_pair, sample_step):
+def odeint(x, text_cfg_dict, sample_step, start_t=14.648, end_t=1e-3):
     ts = latent_sde.get_timesteps(
-        T=sample_step, 
-        sigma_max=latent_sde.sigma_score.scheduler.init_noise_sigma
+        T=sample_step,
+        sigma_max=start_t,
+        sigma_min=end_t
     )
-    conditions = [
-        (get_embedding(prompt), weight) for prompt, weight in text_weight_pair.items()
-    ]
+    text_cfg_dict['prompts'].append('')
+    prompts = {
+        'conditions': get_condition(text_cfg_dict['prompts']),
+        'cfgs': text_cfg_dict['cfgs'],
+    }
     prev_t = ts[0]
     for t in ts[1:]:
-        print(x.norm().item())
-        dt = t - prev_t
-        f1, _ = get_f_g(prev_t, x, conditions)
-        x_pred = x + f1 * dt
-        f2, _ = get_f_g(t, x_pred, conditions)
-        x = x + 0.5 * (f1 + f2) * dt
+        x = ode_step(x, t, prev_t, prompts)
         prev_t = t
     return x
 
 
 @torch.inference_mode()
-def sdeint(x, text_weight_pair, beta, sample_step, start_t=1., end_t=0.):
+def sdeint(x, text_cfg_dict, beta, sample_step, start_t=14.648, end_t=1e-3):
     latent_sde.change_noise(beta=beta)
-    ts = latent_sde.get_timesteps(sample_step, start_t, end_t)
-    prompts = [
-        (get_embedding(prompt), weight) for prompt, weight in text_weight_pair.items()
-    ]
-    prev_t = start_t
-    for t in ts:
-        dt = t - prev_t
-        z = torch.randn_like(x)
-        rand_term = z * torch.sqrt(torch.abs(dt))
-        f1, g1 = get_f_g(prev_t, x, prompts)
-        x_pred = x + f1 * dt + g1 * rand_term
-        f2, g2 = get_f_g(t, x_pred, prompts)
-        x = x + 0.5 * (f1 + f2) * dt + 0.5 * (g1 + g2) * rand_term
+    ts = latent_sde.get_timesteps(
+        T=sample_step,
+        sigma_max=start_t,
+        sigma_min=end_t
+    )
+    text_cfg_dict['prompts'].append('')
+    prompts = {
+        'conditions': get_condition(text_cfg_dict['prompts']),
+        'cfgs': text_cfg_dict['cfgs'],
+    }
+    prev_t = ts[0]
+    for t in ts[1:]:
+        x = sde_step(x, t, prev_t, prompts, torch.randn_like(x))
         prev_t = t
     return x
 
-
 @torch.inference_mode()
-def demon_sampling(x, energy_fn, text_weight_pair, beta, tau, action_num, sample_step, weighting="spin", log_dir=None, start_t=1., end_t=0., ode_after_sigma=0):
+def demon_sampling(x, energy_fn, text_cfg_dict, beta, tau, action_num, sample_step, weighting="spin", log_dir=None, start_t=14.648, end_t=1e-4, max_ode_steps=8, ode_after=0):
     assert x.shape[0] == 1
     latent_sde.change_noise(beta=beta)
-    ts = latent_sde.get_timesteps(sample_step, start_t, end_t)
-    prompts = [
-        (get_embedding(prompt), weight) for prompt, weight in text_weight_pair.items()
-    ]
-    prev_t = start_t
+    ts = latent_sde.get_timesteps(
+        T=sample_step,
+        sigma_max=start_t,
+        sigma_min=end_t
+    )
+    text_cfg_dict['prompts'].append('')
+    prompts = {
+        'conditions': get_condition(text_cfg_dict['prompts']),
+        'cfgs': text_cfg_dict['cfgs'],
+    }
+    prev_t, ts = ts[0], ts[1:]
     while len(ts) > 0:
         t, ts = ts[0], ts[1:]
-        if latent_sde.karras.sigma(t.item()) < ode_after_sigma:
-            return odeint_rest(x, t, ts, prompts)
-            
-        zs = torch.randn(action_num, *x.shape[1:]).to(x.device)
-        next_x = sde_step(x, t, prev_t, prompts, zs)
-        latent_sde.ode_mode()
-        candidate_0 = odeint_rest(next_x, t, ts, prompts)
-        latent_sde.ode_mode_revert()
-
-        values = torch.tensor(energy_fn(candidate_0), dtype=x.dtype, device=x.device)
+        if t < ode_after:
+            x = odeint_rest(x, t, ts, prompts)
+            break
+        zs = torch.randn(action_num, *x.shape[1:], device=x.device, dtype=x.dtype)
+        next_xs = sde_step(x, t, prev_t, prompts, zs)    
+        
+        with OdeModeContextManager():
+            candidates_0 = odeint_rest(next_xs, t, ts, prompts, max_ode_steps=max_ode_steps)
+        
+        values = torch.tensor(energy_fn(candidates_0)).to(device=x.device, dtype=x.dtype)
         
         if log_dir is not None:
             # Append values.mean().item() and values.std().item() to {log_dir}/sample_hist.txt
             with open(f"{log_dir}/expected_energy.txt", "a") as f:
-                f.write(f"{values.mean().item()} {values.std().item()} {latent_sde.karras.sigma(t.item())}\n")
+                f.write(f"{values.mean().item()} {values.std().item()} {t.item()}\n")
 
         values = values - values.mean()
         
         if weighting == "spin":
-            weights = torch.tanh(values / tau).to(x.device)
+            weights = torch.tanh(values / tau)
         elif weighting == "boltzmann":
-            weights = F.softmax(values / tau, dim=0).to(x.device)
+            weights = F.softmax(values / tau, dim=0)
         else:
             raise ValueError(f"Unknown weighting: {weighting}")
 
         z = F.normalize((zs * weights.view(-1, 1, 1, 1)).sum(dim=0, keepdim=True), dim=(0, 1, 2, 3)) # (1, C, H, W)
         z *= x.numel()**0.5
         x = sde_step(x, t, prev_t, prompts, z)
-        prev_t = t.item()
+        prev_t = t
     return x
-
-
 
 def add_noise(latent, t):
     z = torch.randn_like(latent)
-    print(f"Sigma = {latent_sde.karras.sigma(t)}")
-    return latent + z * latent_sde.karras.sigma(t)
+    return latent + z * t
 
 def get_init_latent():
     return latent_sde.prepare_initial_latents()
-
-
