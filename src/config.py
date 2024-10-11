557f93f22b586011b3fe03358aacc5c51384deb9
rareone0602 <rareone0602@gmail.com>
2024-10-11 07:51:39 +0000

    currently SD1.5 as default

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import torch
 DTYPE = torch.float16
-# FILE_PATH = "runwayml/stable-diffusion-v1-5"
+FILE_PATH = "runwayml/stable-diffusion-v1-5"
 C_FILE_PATH = "latent-consistency/lcm-lora-sdv1-5"
-FILE_PATH = "CompVis/stable-diffusion-v1-4"
+# FILE_PATH = "CompVis/stable-diffusion-v1-4"
 DEVICE = torch.device("cuda")
 IMAGE_DIMENSION = 512
